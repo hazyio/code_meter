0bad5847494a728f6490cd0fd7eae874dfee0427
Daniel Enubiaka <danielenubiakamail@gmail.com>
2026-07-18 19:11:54 +0100

    Refactor HomeSubPage layout and adjust padding for improved UI

--- app/lib/pages/home/home.dart
+++ app/lib/pages/home/home.dart
@@ -10,26 +10,19 @@ class HomeSubPage extends StatelessWidget {
     // void goToSettingsPage() {
     //   Navigator.pushNamed(context, '/settings');
     // }
-
     return Padding(
-      padding: const EdgeInsets.all(AppSpacing.marginTablet),
+      padding: const EdgeInsets.only(top: AppSpacing.gutter),
       child: Column(
         children: [
-          // Row(
-          //   mainAxisAlignment: .end,
-          //   children: [
-          //     IconButton(
-          //       icon: const Icon(Icons.settings),
-          //       onPressed: goToSettingsPage,
-          //     ),
-          //   ],
-          // ),
-          Card(
-            shadowColor: Colors.transparent,
-            margin: const .all(8.0),
-            child: SizedBox.expand(
-              child: Center(
-                child: Text('Home', style: theme.textTheme.titleLarge),
+          Expanded(
+            // <-- gives Card a bounded height to expand into
+            child: Card(
+              shadowColor: Colors.transparent,
+              margin: const .all(8.0),
+              child: SizedBox.expand(
+                child: Center(
+                  child: Text('Home', style: theme.textTheme.titleLarge),
+                ),
               ),
             ),
           ),
