ac55f6914938cdc515b47901be9f65a40327c082
Daniel Enubiaka <danielenubiakamail@gmail.com>
2026-07-21 07:22:47 +0100

    feat: update essential apps sources, enhance page layout, and configure Vite adapter

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -12,7 +12,9 @@ export default defineConfig({
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-			adapter: adapter()
+			adapter: adapter({
+				fallback: 'index.html'
+			})
 		})
 	]
 });
